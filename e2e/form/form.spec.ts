2b4b0e8115afacb1129755c80fa4ba43fda43b52
re2osushi8888 <re.2osushi8888@gmail.com>
2024-11-13 08:37:40 +0000

    不要なテストの削除

--- e2e/form/form.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test';
-
-test('test', async ({ page }) => {
-	await page.goto('http://localhost:3000/');
-	await page.getByRole('link', { name: '入力フォーム' }).click();
-	await page.getByRole('heading', { name: '入力フォーム' }).click();
-});
