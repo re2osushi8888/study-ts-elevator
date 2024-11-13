8898bc8f8408b85ebbfa00df8d8a2c769134d9e0
re2osushi8888 <re.2osushi8888@gmail.com>
2024-11-13 08:59:43 +0000

    人が2階でボタンを押すと、階の表示が2になっている

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -5,4 +5,10 @@ test('エレベーターが1階にいる時、階の表示が1になっている
 
 	await expect(page.getByRole('main')).toHaveText(/1F/);
 });
-　
+
+test('人が2階でボタンを押すと、階の表示が2になっている', async ({ page }) => {
+	await page.goto('http://localhost:3000');
+  await page.getByRole('button', { name: '下' }).click();
+
+	await expect(page.getByRole('main')).toHaveText(/2F/);
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-	title: '最初のページ',
-	description: 'Playwrightハンズオンの最初のステップ',
-};
+'use client';
+import { useState } from 'react';
 
 export default function Home() {
+	const [floor, setFloor] = useState('1F');
+	const changeTo2F = () => {
+		setFloor('2F');
+	};
+
 	return (
 		<main>
 			<div>
 				現在の階:
-				<span>1F</span>
+				<span>{floor}</span>
 			</div>
+			<button type={'button'} onClick={changeTo2F}>
+				下
+			</button>
 		</main>
 	);
 }
